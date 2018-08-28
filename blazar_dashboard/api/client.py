c9abe4a56d4b3cb9a57da8eca74fde24f4f7a631
Pierre Riteau <pierre@stackhpc.com>
2018-08-28 15:27:47 +0100

    Adapt MySQL queries to the Blazar database schema in Rocky

--- blazar_dashboard/api/client.py
+++ blazar_dashboard/api/client.py
@@ -27,7 +27,6 @@ from openstack_dashboard.api import base
 from blazarclient import client as blazar_client
 
 
-NOT_DELETED = ''
 LOG = logging.getLogger(__name__)
 LEASE_DATE_FORMAT = "%Y-%m-%d %H:%M"
 
@@ -199,8 +198,7 @@ def compute_host_available(request, start_date, end_date):
             join reservations r on r.id = cha.`reservation_id`
             join leases l on l.`id` = r.`lease_id`
             where
-                r.deleted="" and
-                ch.deleted="" and
+                r.deleted IS NULL and
                 ((l.`start_date` > %s and l.`start_date` < %s)
                 or (l.`end_date` > %s and l.`end_date` < %s)
                 or (l.`start_date` < %s and l.`end_date` > %s))
@@ -224,7 +222,7 @@ def node_in_lease(request, lease_id, active_only=True):
         l.id = %s
     '''
     if active_only:
-        sql += " AND ca.deleted = '{}'".format(NOT_DELETED)
+        sql += " AND ca.deleted IS NULL"
     sql_args = (lease_id,)
 
     cursor = get_cursor_for_request(request)
@@ -245,8 +243,6 @@ def compute_host_list(request, node_types=False):
         hypervisor_type
     FROM
         computehosts
-    WHERE
-        deleted = ""
     '''
     cursor = get_cursor_for_request(request)
     cursor.execute(sql)
@@ -272,7 +268,7 @@ def node_type_map(request=None, cursor=None):
         INNER JOIN (
             SELECT id, MAX(created_at)
             FROM blazar.computehost_extra_capabilities
-            WHERE capability_name = 'node_type' AND deleted = ''
+            WHERE capability_name = 'node_type'
             GROUP BY computehost_id
         ) AS exl
         ON ex.id = exl.id
@@ -302,9 +298,8 @@ def reservation_calendar(request):
         JOIN reservations r ON r.id = cha.reservation_id
         JOIN leases l ON l.id = r.lease_id
     WHERE
-        r.deleted = ''
-        AND c.deleted = ''
-        AND cha.deleted = ''
+        r.deleted IS NULL
+        AND cha.deleted IS NULL
     ORDER BY
         start_date,
         project_id;
@@ -325,10 +320,8 @@ def extra_capability_names(request):
         capability_name
     FROM
         computehost_extra_capabilities
-    WHERE
-        deleted = %s
     '''
-    cursor.execute(sql, [NOT_DELETED])
+    cursor.execute(sql)
     # available = dictfetchall(cursor)
     available = [row[0] for row in cursor.fetchall()]
     return available
@@ -353,8 +346,7 @@ def extra_capability_values(request, name):
         computehost_extra_capabilities
     WHERE
         capability_name = %s
-        AND deleted = %s;
     '''
-    cursor.execute(sql, [name, NOT_DELETED])
+    cursor.execute(sql, [name])
     rows = dictfetchall(cursor)
     return rows
