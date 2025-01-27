25497a477063ee3fb70611391d81e125d4a23a7d
Mark Powers <markpowers@uchicago.edu>
2025-01-27 21:31:57 +0000

    Fix minor issues

--- blazar_dashboard/content/leases/workflows.py
+++ blazar_dashboard/content/leases/workflows.py
@@ -4,7 +4,6 @@ import pytz
 
 from blazar_dashboard import api
 from blazar_dashboard import conf
-from django import template
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from horizon import exceptions
